1f49f7f64273a635e5b6d6423221a60fb7ec9077
frshock <will.s.crane@gmail.com>
2016-09-30 12:49:22 -0400

    Second Iteration of Will's Styling

---
+++ employees/test.js
@@ -0,0 +1,20 @@
+$(".circle").hover(function(){
+    $(this).css({"background-image":"url(https://i.ytimg.com/vi/rJBkcjGJlI4/maxresdefault.jpg)","background-size":"cover","background-position":"center"});
+} 
+,function(){
+    $(this).css({"background-image":"","background-size":""});
+});
+
+$(".btn-social-icon").hover(function(){
+    $(this).css({"background-color":"#2B2E4A","color":"#FFFFFF"});
+}
+,function(){
+    $(this).css({"background-color":"","color":""});
+});
+
+$(".btn-danger").hover(function(){
+    $(this).css({"background-color":"#2B2E4A","color":"#FFFFFF"});
+}
+,function(){
+    $(this).css({"background-color":"","color":""});
+});
